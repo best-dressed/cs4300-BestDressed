a31d1e956b9c4e70970d7cd9cb56427b5656c534
Robin Levisky <robin.levisky@outlook.com>
2025-10-01 20:07:39 -0600

    Adding boilerplate Django code and templates/photos to support a basic landing page.

---
+++ best_dressed/views.py
@@ -0,0 +1,11 @@
+"""
+Django views for the Best Dressed application.
+"""
+
+from django.shortcuts import render
+
+def index(request):
+    """
+    View for the index page, currently just a splash page. 
+    """
+    return render(request, 'index.html')
