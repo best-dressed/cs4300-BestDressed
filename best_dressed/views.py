418c5d88ed05824bd04b4bde304c441d4b16b21c
Logan Andersen <loganandersen@mailfence.com>
2025-10-12 16:23:24 -0600

    make main.html apply global dark mode

--- best_dressed/views.py
+++ best_dressed/views.py
@@ -6,6 +6,18 @@ from django.shortcuts import render
 
 def index(request):
     """
-    View for the index page, currently just a splash page. 
+    View for the index page, as of now this is a landing page.
     """
     return render(request, 'index.html')
+
+def login(request):
+    """
+    View the login page
+    """
+    return render(request, 'login.html')
+
+def signup(request):
+    """
+    View the signup page
+    """
+    return render(request, 'signup.html')
