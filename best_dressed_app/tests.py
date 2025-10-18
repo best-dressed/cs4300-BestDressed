11508da35aab6bfce59d5323cf28746f1eaeccdd
Mason Andersen <masonandersen3002@gmail.com>
2025-10-18 17:22:00 -0600

    add some tests for item_listing view, add coverage to requirements.txt

--- best_dressed_app/tests.py
+++ best_dressed_app/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import *
+from django.urls import reverse
 # Create your tests here.
 
 # test item model creation
@@ -19,3 +20,50 @@ class ItemModelTest(TestCase):
         self.assertEqual(created_item.description, "A mind-bending thrilling T shirt the world has never seen")
         self.assertEqual(created_item.image_url,
                          "https://coloradosprings.gov/sites/default/files/styles/large/public/2024-05/crystal_reservoir.jpg?itok=Z1jcwxG7")
+
+
+# test item listing views
+# Heavily chatGPT based
+# basically just simulate a bunch of requests and look for specific data that must have come from view logic
+class ItemViewsTest(TestCase):
+    def setUp(self):
+
+        # make two dummy items to display on item_listing
+        item = Item.objects.create(
+            title="T shirt",
+            description="A mind-bending thrilling T shirt the world has never seen",
+            image_url = "https://coloradosprings.gov/sites/default/files/styles/large/public/2024-05/crystal_reservoir.jpg?itok=Z1jcwxG7"
+        )
+        item2 = Item.objects.create(
+            title="Pants",
+            description="One for each leg",
+            image_url = "https://ashallendesign.co.uk/images/custom/short-url-logo.png"
+        )
+
+    def test_item_listing_view_status_code(self):
+        """View should return 200 OK"""
+        url = reverse("item_listing")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_item_listing_correct_template(self):
+        """View should render the correct template"""
+        url = reverse("item_listing")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "item_listing.html")
+
+    def test_item_listing_displays_items(self):
+        """View should include data about items in rendered HTML"""
+        url = reverse("item_listing")
+        response = self.client.get(url)
+        self.assertContains(response, "T shirt")
+        self.assertContains(response, "Pants")
+        self.assertContains(response, "for each leg")
+        self.assertContains(response, "https://ashallendesign.co.uk/images/custom/short-url-logo.png")
+
+    def test_item_listing_view_context(self):
+        """View should pass correct context data"""
+        url = reverse("item_listing")
+        response = self.client.get(url)
+        self.assertIn("items", response.context)
+        self.assertEqual(len(response.context["items"]), 2)
