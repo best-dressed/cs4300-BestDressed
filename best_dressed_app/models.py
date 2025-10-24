ded62b06c79582585a571e8efa8bafbf82ea29f5
Mason Andersen <masonandersen3002@gmail.com>
2025-10-24 15:24:37 -0600

    set up add item functionality using forms, add item tags and item short description for display, refactor item templates to use item_card

--- best_dressed_app/models.py
+++ best_dressed_app/models.py
@@ -5,18 +5,40 @@ from django.urls import reverse
 
 # article of clothing item
 class Item(models.Model):
+
+    ITEM_TAG_CHOICES = {
+        "Accessory": "accessory",
+        "Torso": "torso",
+        "Head": "head",
+        "Legs": "legs",
+        "Shoes": "shoes"
+    }
+
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
 
     # for main item listing so everything is more clean with user-added data
-    #short_description = models.CharField(max_length=50)
-    image_url = models.URLField(max_length=2000, default="https://pangaia.com/cdn/shop/files/DNA_Oversized_T-Shirt_-Summit_Blue-1.png?crop=center&height=1999&v=1755260238&width=1500")
-    #tag = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=75, default="")
+    image_url = models.URLField(max_length=2000, default="")
+    tag = models.CharField(max_length=20, choices=ITEM_TAG_CHOICES, default="")
+
+    # make it so short description created automatically from description
+    def save(self, *args, **kwargs):
+        if self.description:
+            # if description is long, add ...
+            if len(self.description) > 75:
+                self.short_description = (self.description[:72] + "...")
+
+            # otherwise just make it the same bc doesn't matter.
+            else:
+                self.short_description = self.description[:75]
+
+        # if user put nothing, put nothing
+        else:
+            self.short_description = ""
+        super().save(*args, **kwargs)
 
     # set up url for viewing a particular item
     def get_absolute_url(self):
         return reverse("item_detail", kwargs={'pk': self.pk})
 
-    # set up short description
-    def get_short_description(self):
-        return self.description[:50]
