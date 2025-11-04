4bb95bc8ac2fce5443d4f57faf36877787ea570c
Logan Andersen <landerse@uccs.edu>
2025-11-04 00:51:14 +0000

    added views and forms

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Thread(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='threads')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Thread: {self.title} by {self.user.username}"
+
+
+
+# items saved by a user to their wardrobe
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.user.username} in {self.thread.title}"
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Thread, Post
+from .forms import PostForm  # assuming you have a form for creating posts
 
 # Create your views here.
+@login_required
+def create_post(request, thread_id):
+    thread = get_object_or_404(Thread, id=thread_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.thread = thread
+            post.user = request.user
+            post.save()
+            return redirect('thread_detail', thread_id=thread.id)
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form, 'thread': thread})
