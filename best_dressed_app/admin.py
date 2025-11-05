515be09679c40bd8d68389ee4d55b1d83357389b
John-Camargo <jecamargo2003@hotmail.com>
2025-11-05 03:11:48 +0000

    Adding Outfit Model and ability to manage/edit outfits in admin panel (along with wardrobe items).

--- best_dressed_app/admin.py
+++ best_dressed_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Item, UserProfile
+from .models import Item, UserProfile, WardrobeItem, Outfit
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(UserProfile)
+admin.site.register(WardrobeItem)
+admin.site.register(Outfit)
