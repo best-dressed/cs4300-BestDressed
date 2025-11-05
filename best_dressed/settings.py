0cd12bfda743d870be7a0496607dfc68895c0760
Michal Lustig <lmichal43@gmail.com>
2025-11-05 02:38:37 +0000

    fixed error for account test

--- best_dressed/settings.py
+++ best_dressed/settings.py
@@ -145,7 +145,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Auth: redirects after login/logout
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "index"
-LOGIN_URL = "login"
+LOGIN_URL = "/accounts/login/"
 
 
 # For testing password reset emails locally
