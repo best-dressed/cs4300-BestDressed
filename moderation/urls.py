e7cdc7e5b7a5c75c69f5e21ea8791bf77613d6fb
Logan Andersen <landerse@uccs.edu>
2025-11-17 20:18:28 +0000

    added ip bans

---
+++ moderation/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # IP ban page
+    path("ip_ban/", views.ip_ban_page, name="ip_ban"),
+
+    # Filtered content page (generic moderation)
+    path("filtered_content/", views.filtered_content_message, name="filtered_content"),
+]
