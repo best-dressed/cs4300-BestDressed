7cf62c720d5c9fde0544948bf4ce4c0e66385087
Logan Andersen <loganandersen@mailfence.com>
2025-12-07 09:16:07 -0700

    fixed unterminated literal error

--- best_dressed/settings.py
+++ best_dressed/settings.py
@@ -116,8 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': '
-        django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME':
